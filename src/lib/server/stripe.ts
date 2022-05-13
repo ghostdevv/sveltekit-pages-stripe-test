66e55e34650e79098d0966ebf361069dabc77bff
GHOSt <ghostdevbusiness@gmail.com>
2022-05-13 17:20:13 +0100

    test

--- src/lib/server/stripe.ts
+++ src/lib/server/stripe.ts
@@ -1,6 +1,8 @@
-import Stripe from 'stripe';
+export const createStripe = async () => {
+    const { default: Stripe } = await import('stripe');
 
-export const stripe = new Stripe(import.meta.env.VITE_STRIPE_KEY, {
-    apiVersion: '2020-08-27',
-    httpClient: Stripe.createFetchHttpClient(),
-});
+    return new Stripe(import.meta.env.VITE_STRIPE_KEY, {
+        apiVersion: '2020-08-27',
+        httpClient: Stripe.createFetchHttpClient(),
+    });
+};
--- src/routes/api/customers.ts
+++ src/routes/api/customers.ts
@@ -1,6 +1,7 @@
-import { stripe } from '$lib/server/stripe';
+import { createStripe } from '$lib/server/stripe';
 
 export const get = async () => {
+    const stripe = await createStripe();
     const res = await stripe.customers.list();
 
     const names = res.data.map((c) => c.name);
